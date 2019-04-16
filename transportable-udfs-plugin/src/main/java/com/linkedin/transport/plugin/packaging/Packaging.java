38b4817490ef528269e120bbfd935a330dcf43a7
Shardul Mahadik <smahadik@linkedin.com>
2019-04-15 18:09:06 -0700

    Fixed license issues

--- transportable-udfs-plugin/src/main/java/com/linkedin/transport/plugin/packaging/Packaging.java
+++ transportable-udfs-plugin/src/main/java/com/linkedin/transport/plugin/packaging/Packaging.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2019 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.transport.plugin.packaging;
 
 import com.linkedin.transport.plugin.Platform;
