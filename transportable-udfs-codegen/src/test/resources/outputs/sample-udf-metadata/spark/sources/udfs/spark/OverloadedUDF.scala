af04353de856dbb52276bff128c1dfd3dd4c136b
Shardul Mahadik <smahadik@linkedin.com>
2019-05-02 11:39:49 -0700

    Fix Spark shading issues (#20)

--- transportable-udfs-codegen/src/test/resources/outputs/sample-udf-metadata/spark/sources/udfs/spark/OverloadedUDF.scala
+++ transportable-udfs-codegen/src/test/resources/outputs/sample-udf-metadata/spark/sources/udfs/spark/OverloadedUDF.scala
@@ -3,7 +3,8 @@ package udfs.spark
 import java.util
 import com.google.common.collect.ImmutableList
 import com.linkedin.transport.api.udf.{StdUDF, TopLevelStdUDF}
-import com.linkedin.transport.spark.StdUdfWrapper
+import com.linkedin.transport.spark.{SparkStdUDF, StdUDFRegistration, StdUdfWrapper}
+import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.expressions.Expression
 
 
@@ -14,4 +15,15 @@ case class OverloadedUDF(expressions: Seq[Expression]) extends StdUdfWrapper(exp
   override protected def getStdUdfImplementations: util.List[_ <: StdUDF] = ImmutableList.of(
     new udfs.OverloadedUDFInt(), new udfs.OverloadedUDFString()
   )
+}
+
+object OverloadedUDF {
+
+  def register(name: String): SparkStdUDF = {
+    StdUDFRegistration.register(name, classOf[OverloadedUDF])
+  }
+
+  def register(name: String, session: SparkSession): SparkStdUDF = {
+    StdUDFRegistration.register(name, classOf[OverloadedUDF], session)
+  }
 }
