cfefccafa5182ba3720003579113196dbca18e64
Raymond Zhang <razhang@linkedin.com>
2022-04-20 15:04:22 -0700

    Revert "Only call getRequiredFiles on spark driver (#115)"
    This reverts commit 7ea26742153be8241dcd098868925f7543967cc7.

--- transportable-udfs-spark_2.11/src/main/scala/com/linkedin/transport/spark/StdUdfWrapper.scala
+++ transportable-udfs-spark_2.11/src/main/scala/com/linkedin/transport/spark/StdUdfWrapper.scala
@@ -13,7 +13,7 @@ import com.linkedin.transport.api.data.{PlatformData, StdData}
 import com.linkedin.transport.api.udf._
 import com.linkedin.transport.spark.typesystem.SparkTypeInference
 import com.linkedin.transport.utils.FileSystemUtils
-import org.apache.spark.{SparkFiles, TaskContext}
+import org.apache.spark.SparkFiles
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.Expression
@@ -47,10 +47,7 @@ abstract class StdUdfWrapper(_expressions: Seq[Expression]) extends Expression
     _stdUdf = _sparkTypeInference.getStdUdf
     _nullableArguments = _stdUdf.getAndCheckNullableArguments
     _stdUdf.init(_stdFactory)
-    // Only call this on driver
-    if (TaskContext.get == null) {
-      getRequiredFiles()
-    }
+    getRequiredFiles()
     _requiredFilesProcessed = false
     _initialized = true
   }
