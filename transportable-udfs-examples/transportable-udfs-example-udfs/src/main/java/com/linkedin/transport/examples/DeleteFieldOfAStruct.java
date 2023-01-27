85415670053d999d732baaf42b81625314d08f48
Mohit Verma <vmohit.93@gmail.com>
2023-01-26 16:12:59 -0800

    Add license header

--- transportable-udfs-examples/transportable-udfs-example-udfs/src/main/java/com/linkedin/transport/examples/DeleteFieldOfAStruct.java
+++ transportable-udfs-examples/transportable-udfs-example-udfs/src/main/java/com/linkedin/transport/examples/DeleteFieldOfAStruct.java
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2023 LinkedIn Corporation. All rights reserved.
+ * Licensed under the BSD-2 Clause license.
+ * See LICENSE in the project root for license information.
+ */
 package com.linkedin.transport.examples;
 
 import com.google.common.collect.ImmutableList;
