9d394df22046244677cd4ef69a58d0d27fb4ae91
ramurm2013-droid <ramu.rm2013@gmail.com>
2025-10-03 15:40:02 -0700

    Add gradle.properties (#160)
    * Add gradle.properties
    
    * Remove TestNestedMapFromTwoArraysFunction.java
    
    ---------
    
    Co-authored-by: Ramanathan Ramu <rramu@linkedin.biz>

--- transportable-udfs-examples/transportable-udfs-example-udfs/src/test/java/com/linkedin/transport/examples/TestNestedMapFromTwoArraysFunction.java
+++ transportable-udfs-examples/transportable-udfs-example-udfs/src/test/java/com/linkedin/transport/examples/TestNestedMapFromTwoArraysFunction.java
@@ -10,10 +10,8 @@ import com.google.common.collect.ImmutableMap;
 import com.linkedin.transport.api.udf.StdUDF;
 import com.linkedin.transport.api.udf.TopLevelStdUDF;
 import com.linkedin.transport.test.AbstractStdUDFTest;
-import com.linkedin.transport.test.spi.StdTester;
 import java.util.List;
 import java.util.Map;
-import org.testng.annotations.Test;
 
 
 public class TestNestedMapFromTwoArraysFunction extends AbstractStdUDFTest {
@@ -21,27 +19,5 @@ public class TestNestedMapFromTwoArraysFunction extends AbstractStdUDFTest {
   @Override
   protected Map<Class<? extends TopLevelStdUDF>, List<Class<? extends StdUDF>>> getTopLevelStdUDFClassesAndImplementations() {
     return ImmutableMap.of(NestedMapFromTwoArraysFunction.class, ImmutableList.of(NestedMapFromTwoArraysFunction.class));
-  }
-
-  @Test
-  public void testNestedMapUnionFunction() {
-    // in case of Trino v446, the output of the query with UDF "udf_map_from_two_arrays" is "array(array(map(...)))
-    // in case of Hive and Spark, the output of the query with UDF "udf_map_from_two_arrays" is "array(row(map(...)))
-    StdTester tester = getTester();
-    tester.check(
-        functionCall("nested_map_from_two_arrays", array(row(array(1, 2), array("a", "b")))),
-        isTrinoTest() ? array(array(map(1, "a", 2, "b"))) : array(row(map(1, "a", 2, "b"))),
-        "array(row(map(integer,varchar)))");
-    tester.check(
-        functionCall("nested_map_from_two_arrays", array(row(array(1, 2), array("a", "b")), row(array(11, 12), array("aa", "bb")))),
-        isTrinoTest() ?  array(array(map(1, "a", 2, "b")), array(map(11, "aa", 12, "bb")))
-            : array(row(map(1, "a", 2, "b")), row(map(11, "aa", 12, "bb"))),
-        "array(row(map(integer,varchar)))");
-    tester.check(
-        functionCall("nested_map_from_two_arrays",  array(row(array(1), array("a", "b")))),
-        null, "array(row(map(integer,varchar)))");
-    tester.check(
-        functionCall("nested_map_from_two_arrays",  array(row(null, array("a", "b")))),
-        null, "array(row(map(unknown,varchar)))");
   }
 }
